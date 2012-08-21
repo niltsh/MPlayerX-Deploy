298a67a39b861b24749415f55a90b2513735aedb
Zongyao QU <zongyao.qu@gmail.com>
2012-08-21 22:52:36 +0900

    start to use google code for file hosting

---
+++ upload-googlecode.sh
@@ -0,0 +1,18 @@
+
+PWD=`pwd`
+
+if [[ $# != 1 ]]; then
+	echo "./upload-googlecode.sh [版本编号]"
+	exit
+fi
+
+BIN=$PWD/releases/MPlayerX-$1.zip
+
+if [[ -f $BIN ]]; then
+    GCUSER=support@mplayerx.org
+	GCPSWD=`security find-generic-password -w -s "MPlayerX-GoogleCode"`
+
+	./scripts/googlecode_upload.py -p mplayerx -s $1 -u $GCUSER -w $GCPSWD $BIN
+else
+	echo "$BIN does not exists."
+fi
