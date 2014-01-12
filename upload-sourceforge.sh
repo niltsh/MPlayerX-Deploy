b1ec7fc74cba83859ec6baf7993f8a49a885df25
Zongyao Qu <zongyao.qu@gmail.com>
2014-01-12 17:10:49 +0900

    change release binaries to source forge

---
+++ upload-sourceforge.sh
@@ -0,0 +1,20 @@
+
+PWD=`pwd`
+
+if [[ $# != 1 ]]; then
+	echo "./upload-sourceforge.sh [版本编号]"
+	exit
+fi
+
+BIN=$PWD/releases/MPlayerX-$1.zip
+
+if [[ -f $BIN ]]; then
+    SFUSER=quzongyao
+    SFPROJ=mplayerx-osx
+
+    scp ${BIN} ${SFUSER}@frs.sourceforge.net:/home/frs/project/${SFPROJ}
+
+	md5 $BIN
+else
+	echo "$BIN does not exists."
+fi
